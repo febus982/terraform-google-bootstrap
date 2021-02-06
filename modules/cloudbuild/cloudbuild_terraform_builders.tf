10c7b44e108350ae9aa4481f7730d8407835093c
Federico Busetti <729029+febus982@users.noreply.github.com>
2021-02-06 14:57:33 +0000

    Add multiple terraform cloud builder versions

---
+++ modules/cloudbuild/cloudbuild_terraform_builders.tf
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ARG TERRAFORM_VERSION=0.13.6
+#ARG TERRAFORM_VERSION_SHA256SUM=55f2db00b05675026be9c898bdd3e8230ff0c5c78dd12d743ca38032092abfc9
+//ARG TERRAFORM_VALIDATOR_RELEASE=2020-03-05
+
+//ARG TERRAFORM_VERSION=0.14.5
+//ARG TERRAFORM_VERSION_SHA256SUM=2899f47860b7752e31872e4d57b1c03c99de154f12f0fc84965e231bc50f312f
+
+#ARG TERRAFORM_VERSION=0.12.29
+#ARG TERRAFORM_VERSION_SHA256SUM=872245d9c6302b24dc0d98a1e010aef1e4ef60865a2d1f60102c8ad03e9d5a1d
+
+
+variable "builders_tag_map" {
+  type    = map(object({
+      terraform_version           = string,
+      terraform_version_sha256sum = string,
+      terraform_validator_release = string,
+  }))
+  default = {
+    "0.12" = {
+      terraform_version = "0.12.29",
+      terraform_version_sha256sum = "872245d9c6302b24dc0d98a1e010aef1e4ef60865a2d1f60102c8ad03e9d5a1d",
+      terraform_validator_release = "2020-09-24",
+    },
+    "0.13" = {
+      terraform_version = "0.13.6",
+      terraform_version_sha256sum = "55f2db00b05675026be9c898bdd3e8230ff0c5c78dd12d743ca38032092abfc9",
+      terraform_validator_release = "2020-09-24",
+    },
+    "0.14" = {
+      terraform_version = "0.14.5",
+      terraform_version_sha256sum = "2899f47860b7752e31872e4d57b1c03c99de154f12f0fc84965e231bc50f312f",
+      terraform_validator_release = "2020-09-24",
+    }
+  }
+}
+
+
+/***********************************************
+ Cloud Build - Terraform builder
+ ***********************************************/
+
+resource "null_resource" "custom_cloudbuild_terraform_builder" {
+  for_each = var.builders_tag_map
+  
+  triggers = {
+    project_id_cloudbuild_project = module.cloudbuild_project.project_id
+    terraform_version_sha256sum   = each.value.terraform_version_sha256sum
+    terraform_version             = each.value.terraform_version
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud builds submit ${path.module}/cloudbuild_builder/ \
+      --project ${module.cloudbuild_project.project_id} \
+      --config=${path.module}/cloudbuild_builder/custom_cloudbuild.yaml \
+      --substitutions=_IMAGE_TAG=${each.key},_TERRAFORM_VERSION=${each.value.terraform_version},_TERRAFORM_VERSION_SHA256SUM=${each.value.terraform_version_sha256sum},_TERRAFORM_VALIDATOR_RELEASE=${each.value.terraform_validator_release}
+  EOT
+  }
+  depends_on = [
+    google_project_service.cloudbuild_apis,
+  ]
+}
+
